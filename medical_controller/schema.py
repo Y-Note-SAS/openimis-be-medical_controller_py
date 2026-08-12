498b96e76c86da404a2207df44e165913c3ccb2d
kevel choumene <k.choumene@cloudfacile.com>
2026-08-12 12:47:17 +0100

    feat: ajustement schema et migrations

--- medical_controller/schema.py
+++ medical_controller/schema.py
@@ -7,35 +7,56 @@ import graphene_django_optimizer as gql_optimizer
 from .apps import MedicalControllerConfig
 from django.core.exceptions import PermissionDenied
 from django.utils.translation import gettext as _
-
-class MissionHFGQLType(DjangoObjectType):
-
-    class Meta:
-        model = MissionHealthFacility
+from core import prefix_filterset, ExtendedConnection
+from location.schema import HealthFacilityGQLType, LocationGQLType
+from core.schema import UserGQLType
 
 class MissionGQLType(DjangoObjectType):
 
+    client_mutation_id = graphene.String()
+
     class Meta:
         model = MedicalControlMission
+        interfaces = (graphene.relay.Node,)
+        filter_fields = {
+            "id": ["exact"],
+            "mission_code": ["exact", "istartswith", "icontains", "iexact"],
+            "status": ["exact", "gt"],
+            "start_date": ["exact", "lt", "lte", "gt", "gte"],
+            "end_date": ["exact", "lt", "lte", "gt", "gte"],
+            **prefix_filterset("region__", LocationGQLType._meta.filter_fields),
+            **prefix_filterset("district__", LocationGQLType._meta.filter_fields),
+            **prefix_filterset("user__", UserGQLType._meta.filter_fields),
+        }
+        connection_class = ExtendedConnection
+
+
+class MissionHFGQLType(DjangoObjectType):
+
+    client_mutation_id = graphene.String()
+
+    class Meta:
+        model = MissionHealthFacility
+        interfaces = (graphene.relay.Node,)
+        filter_fields = {
+            "id": ["exact"],
+            **prefix_filterset("mission__", MissionGQLType._meta.filter_fields),
+            **prefix_filterset("health_facility__", HealthFacilityGQLType._meta.filter_fields),
+        }
+        connection_class = ExtendedConnection
+
 
 class Query(graphene.ObjectType):
 
     missions = OrderedDjangoFilterConnectionField(
         MissionGQLType,
-        region_id=graphene.Int(),
-        district_id=graphene.Int(),
-        status=graphene.String(),
-        mission_code=graphene.String(),
-        start_date=graphene.Date(),
-        end_date=graphene.Date(),
+        orderBy=graphene.List(of_type=graphene.String),
         category=graphene.String(),
         percentage=graphene.String()
     )
 
     mission = graphene.Field(
-        MissionGQLType,
-        id=graphene.Int(),
-        uuid=graphene.UUID()
+        MissionGQLType
     )
 
 def resolve_mission(self, info, **kwargs):
