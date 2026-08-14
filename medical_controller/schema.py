84b199258f05548cecd747651ccd32eba0d44e94
kevel choumene <k.choumene@cloudfacile.com>
2026-08-14 11:44:18 +0100

    feat: ajustement et rajout query qui récupere les controlleurs médicaux ref37552

--- medical_controller/schema.py
+++ medical_controller/schema.py
@@ -10,7 +10,9 @@ from django.utils.translation import gettext as _
 from core import prefix_filterset, ExtendedConnection
 from location.schema import HealthFacilityGQLType, LocationGQLType
 from core.schema import UserGQLType
+from core.models import User
 from.gql_mutations import CreateMissionMutation, UpdateMissionMutation
+from django.contrib.auth import get_user_model
 
 class MissionGQLType(DjangoObjectType):
 
@@ -56,60 +58,56 @@ class Query(graphene.ObjectType):
         percentage=graphene.String()
     )
 
-    mission = graphene.Field(
-        MissionGQLType
+    medical_controllers = OrderedDjangoFilterConnectionField(
+        UserGQLType
     )
 
-def resolve_mission(self, info, **kwargs):
-    if (
-        not info.context.user.has_perms(
-            MedicalControllerConfig.gql_mutation_medical_controller_perms
-        )
-    ):
-        raise PermissionDenied(_("unauthorized"))
+    def resolve_medical_controllers(self, info, search=None, **kwargs):
+        if not info.context.user.has_perms(
+            MedicalControllerConfig.gql_mutation_medical_controller_perms):
+            raise PermissionDenied(_("unauthorized"))
 
-    if kwargs.get("mission_code"):
-        return MedicalControlMission.objects.get(
-            code=kwargs["mission_code"]
-        )
+        return User.objects.filter(
+            validity_to__isnull=True,
+            i_user__user_roles__role__rights__right_id=112000
+        ).distinct()
 
-    return None
 
-def resolve_missions(self, info, **kwargs):
-    if (
-        not info.context.user.has_perms(
-            MedicalControllerConfig.gql_mutation_medical_controller_perms
-        )
-    ):
-        raise PermissionDenied(_("unauthorized"))
+    def resolve_missions(self, info, **kwargs):
+        if (
+            not info.context.user.has_perms(
+                MedicalControllerConfig.gql_mutation_medical_controller_perms
+            )
+        ):
+            raise PermissionDenied(_("unauthorized"))
 
-    query = MedicalControlMission.objects.all()
+        query = MedicalControlMission.objects.all()
 
-    filters = []
+        filters = []
 
-    region_id = kwargs.get("region_id")
-    district_id = kwargs.get("district_id")
-    status = kwargs.get("status")
-    mission_code = kwargs.get("mission_code")
+        region_id = kwargs.get("region_id")
+        district_id = kwargs.get("district_id")
+        status = kwargs.get("status")
+        mission_code = kwargs.get("mission_code")
 
-    if region_id:
-        filters.append(Q(region_id=region_id))
+        if region_id:
+            filters.append(Q(region_id=region_id))
 
-    if district_id:
-        filters.append(Q(district_id=district_id))
+        if district_id:
+            filters.append(Q(district_id=district_id))
 
-    if status:
-        filters.append(Q(status=status))
+        if status:
+            filters.append(Q(status=status))
 
-    if mission_code:
-        filters.append(Q(
-            mission_code__icontains=mission_code
-        ))
+        if mission_code:
+            filters.append(Q(
+                mission_code=mission_code
+            ))
 
-    if filters:
-        query = query.filter(*filters)
+        if filters:
+            query = query.filter(*filters)
 
-    return gql_optimizer.query(query, info)
+        return gql_optimizer.query(query, info)
 
 class Mutation(graphene.ObjectType):
     create_mission = CreateMissionMutation.Field()
